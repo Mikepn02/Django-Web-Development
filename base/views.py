780b4a76815bed0a6c05363d522da4bdcce26163
Mikepn02 <121401821+Mikepn02@users.noreply.github.com>
2023-09-18 08:57:11 +0200

    [done_with_message_functionality]

--- base/views.py
+++ base/views.py
@@ -87,6 +87,7 @@ def room(request, pk):
             room=room,
             body= request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room',pk=room.id)
     context = {"room": room,"room_messages":room_messages,"participants":participants}
     return render(request, "base/room.html", context)
@@ -127,3 +128,13 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message= Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("You are not allowed here")
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": message})
